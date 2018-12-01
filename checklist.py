b898147fd4bb383b17427844ef820058f5736da8
betsybaileyy <betsy.bailey80@gmail.com>
2018-11-30 20:07:59 -0800

    WISHLIST WORKING! :tada:

--- checklist.py
+++ checklist.py
@@ -1,3 +1,7 @@
+from os import system, name
+
+red      = "\033[1;31m"
+green    = "\033[0;32m"
 
 checklist = list()
 
@@ -5,11 +9,10 @@ def create(item):
     checklist.append(item)
 
 def read(index):
-    item = checklist[index]
-    return item
+    return checklist[index]
 
 def update(index, item):
-    checklist[index] = item
+    checklist[int(index)] = item
 
 def destroy(index):
     checklist.pop(index)
@@ -17,31 +20,63 @@ def destroy(index):
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print("{} {}".format(index, list_item))
+        print("   {}     {}".format(index, list_item)) #!!
         index += 1
 
 def mark_completed(index):
     checklist[index] = '√' + checklist[index]
+    print(checklist[index])
+def unmark_completed(index):
+    checklist[int(index)] = checklist[int(index)].replace('√', '')
+    print(checklist[index])
 
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
+def clear():
+    if name == 'nt':
+        _=system('cls')
+    else:
+        _=system('clear')
+
 def select(function_code):
     if function_code == "C":
-        input_item = user_input("Input item:")
+        input_item = user_input( red + "Input item to add to the list: " + green)
         create(input_item)
+        print("\n   " + red + "YOUR LIST" + green + "\n index#  item")
+        list_all_items()
 
     elif function_code == "R":
-        item_index = user_input("Index Number?")
+        item_index = user_input("What is the index number of the item you would like to examine? ")
+        print(read(int(item_index)))
 
-        read(item_index)
+    elif function_code == "U":
+        item_index = user_input("What is the index number of the item you would like to update? ")
+        input_item = input("What item would you like to replace it with? ")
+        update(item_index, input_item)
+        list_all_items()
+
+    elif function_code == "M":
+        item_index = user_input("What is the index number of the item you would like to check as complete? ")
+        mark_completed(int(item_index))
+        list_all_items()
+
+    elif function_code == "UC":
+        item_index = user_input("What is the index number of the item you would like to uncheck as complete? ")
+        unmark_completed(int(item_index))
+        list_all_items()
+
+    elif function_code == "D":
+        index = int(user_input("What is the index number of the item you would like to delete? "))
+        destroy(index)
+        list_all_items()
 
     elif function_code == "P":
         list_all_items()
 
-    else:
-        print("Unknown Option")
+    # else:
+    #     print("Unknown Option")
     return True
 
 def test():
@@ -69,8 +104,13 @@ def test():
     print(user_value)
 
 # test()
+# mark_completed(index)
+print( red + "\nWelcome to your Christmas Wishlist! \nThis thing will come in handy before Santa and his reindeer come to see you. Let's get started." + green + "\nThis list has lots of ways to help you get your wishlist get orginized before it goes to the North Pole! \nLet's look at them: ")
+selection = user_input
+select(selection)
 
 running = True
 while running:
-    selection = user_input("Press C to add to list, R to Read from list and P to display list")
+    selection = user_input("\nC = Create New Item \nR = Examine One Item \nP = Display List \nU = Update Spefic Item \nM = Check Item Off List \nUC = Uncheck Item on List \nD = Delete an Item from your List" + red + "\n\nHow would you like to proceed?" + green + " Input: ")
+    clear()
     running = select(selection)
---
+++ code_along.py
@@ -0,0 +1,15 @@
+#lists
+items = list()
+#or
+items = ['socks', 'belt', 'shirt']
+
+#to change value
+clothes[1, 'neck']
+
+if clothes[0] == 'socks':
+    print('socks are first')
+
+#loops
+while
+
+for
